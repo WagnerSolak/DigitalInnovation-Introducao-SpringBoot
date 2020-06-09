d0db512986b1102277152fed508d7979bc6b5a4b
WagnerSolak <wwwagnerwww@gmail.com>
2020-06-09 14:56:23 -0300

    Auto Configuration

---
+++ src/main/java/com/digitalinnovationone/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // identifica um controler como REST
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloMessage(){
+        return "Hello, configuração está ok !";
+    }
+}
