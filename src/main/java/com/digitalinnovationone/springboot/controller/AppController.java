60e3744c11bc6c740e7a1590684c145d6f0e0627
WagnerSolak <wwwagnerwww@gmail.com>
2020-06-10 12:28:14 -0300

    Setando var ambiente por command line

--- src/main/java/com/digitalinnovationone/springboot/controller/AppController.java
+++ src/main/java/com/digitalinnovationone/springboot/controller/AppController.java
@@ -10,8 +10,17 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NENHUMA}") // caso não tenha nenhuma ele assume o valor NENHUMA
+    private String dbEnvironmentVariable;
+
     @GetMapping("/") // serve para fazer a exibição direta no brownser
     public String getAppMessage(){
         return appMessage;
+    }
+
+    //fazer exibição no browser
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable(){
+        return "A seguinte variável de ambiente foi passada:  " + dbEnvironmentVariable;
     }
 }
